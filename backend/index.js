9833e105c1e33844e50e0819a4cc8a61d29b5bc9
Macarena Huarte <macaahuarte09@gmail.com>
2026-06-10 20:06:35 -0300

    agrego de rutas y controller de canchas

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const canchasRoutes = require('./src/routes/canchas');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/canchas', canchasRoutes);
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ mensaje: 'Bienvenido a Cancha APP' });
+  res.json({ mensaje: 'Bienvenidos a App de turnos de CANCHA' });
 });
 
 app.listen(PORT, () => {
